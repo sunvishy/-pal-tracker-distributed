1137108f17a63e03fa362194ae1d6df033f80e8d
Vishnu <vishnu.sundaramoorthy@cognizant.com>
2020-04-27 13:42:00 -0500

    Security config changes

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/SecurityConfiguration.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package io.pivotal.pal.tracker.allocations;
+package io.pivotal.pal.tracker.timesheets;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +12,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .anyRequest().permitAll()
-            .and()
-            .csrf().disable();
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
     }
-
 }
